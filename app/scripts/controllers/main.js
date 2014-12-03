bef86d33c1d88809fd33def74b33f8fa8fb61348
mjs2020 <me@fm.to.it>
2014-12-03 14:56:13 +0000

    Updates to main controller

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,5 +1,7 @@
 define(['angular', 'pocket-api', 'jquery', 'jquery-cookie', 'oauthpopup'], function (angular, pocket, $) {
   'use strict';
+  // For development only:
+  var baseUrl = 'http://play.fm.to.it/pocketviz/';
 
   /**
    * @ngdoc function
@@ -10,23 +12,25 @@ define(['angular', 'pocket-api', 'jquery', 'jquery-cookie', 'oauthpopup'], funct
    */
   angular.module('pocketvizApp.controllers.MainCtrl', [])
   .controller('MainCtrl', function ($scope) {
-    // Check authentication status
-    if($.cookie('requestToken')) {
-      window.location.hash = "/login";                // go to /login and get accessToken
-      return;
+    // Check for localstorage support with modernizr
+    if(!Modernizr.localstorage) {
+      // emit alert
+      console.log('No localstorage support in browser!');
     }
-    if ($.cookie('accessToken')) {
-      window.location.hash = "/viz";                  // go to /viz and get accessToken
+
+    // Check authentication status
+    if($.cookie('requestToken') || $.cookie('accessToken')) {
+      window.location.hash = "/login";                 // go to /login and get accessToken or readsList
       return;
     }
 
     // Assign function to button
     $scope.startAuth = function () {
-      pocket.getRequestToken(function (err, data) {  // Get request token
+      pocket.getRequestToken(function (err, data) {   // Get request token
         // TODO handle error
         var requestToken = data.code;
         $.oauthpopup({
-          path: 'https://getpocket.com/auth/authorize?request_token='+requestToken+'&redirect_uri=http://play.fm.to.it/ReadsViz/close.html',
+          path: 'https://getpocket.com/auth/authorize?request_token='+requestToken+'&redirect_uri='+baseUrl+'callback.html',
           callback: function oauthCallback () {
             console.log('Popup closed, proceed to authenticating.');
             window.location.hash = "/login";          // go to /login and get accessToken
