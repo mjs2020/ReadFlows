987fb52647574ce84a48c5ab3cc14eead8d67eef
mjs2020 <me@fm.to.it>
2014-12-03 00:09:37 +0000

    Added dependency on jquery to oauthpopup.js

--- app/scripts/oauthpopup.js
+++ app/scripts/oauthpopup.js
@@ -1,11 +1,11 @@
 // Credit: http://www.sitepoint.com/oauth-popup-window/
-define([''], function() {
+define(['jquery'], function($) {
   $.oauthpopup = function(options) {
       options.windowName = options.windowName ||  'ConnectWithOAuth'; // should not include space for IE
       options.windowOptions = options.windowOptions || 'location=0,status=0,width=430,height=700';
       options.callback = options.callback || function(){ window.location.reload(); };
       var that = this;
-      console.log(options.path);
+      // console.log(options.path);
       that._oauthWindow = window.open(options.path, options.windowName, options.windowOptions);
       that._oauthInterval = window.setInterval(function(){
           if (that._oauthWindow.closed) {
