7be20de2967b75dc1ea5581d00b2c179c243fe8b
mjs2020 <me@fm.to.it>
2014-12-03 12:50:52 +0000

    Added modernizr and grunt-modernizr support.

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,11 +21,75 @@ module.exports = function (grunt) {
     dist: 'dist'
   };
 
+  grunt.loadNpmTasks("grunt-modernizr");
+
   // Define the configuration for all the tasks
   grunt.initConfig({
 
     // Project settings
     yeoman: appConfig,
+
+    modernizr: {
+
+      dist: {
+        // [REQUIRED] Path to the build you're using for development.
+        "devFile" : "app/scripts/modernizr.js",
+
+        // Path to save out the built file.
+        "outputFile" : "dist/scripts/modernizr.js",
+
+        // Based on default settings on http://modernizr.com/download/
+        "extra" : {
+          "shiv" : true,
+          "printshiv" : false,
+          "load" : true,
+          "mq" : false,
+          "cssclasses" : true
+        },
+
+        // Based on default settings on http://modernizr.com/download/
+        "extensibility" : {
+          "addtest" : false,
+          "prefixed" : false,
+          "teststyles" : false,
+          "testprops" : false,
+          "testallprops" : false,
+          "hasevents" : false,
+          "prefixes" : false,
+          "domprefixes" : false,
+          "cssclassprefix": ""
+        },
+
+        // By default, source is uglified before saving
+        "uglify" : true,
+
+        // Define any tests you want to implicitly include.
+        "tests" : [],
+
+        // By default, this task will crawl your project for references to Modernizr tests.
+        // Set to false to disable.
+        "parseFiles" : true,
+
+        // When parseFiles = true, this task will crawl all *.js, *.css, *.scss and *.sass files,
+        // except files that are in node_modules/.
+        // You can override this by defining a "files" array below.
+        // "files" : {
+        // "src": []
+        // },
+
+        // This handler will be passed an array of all the test names passed to the Modernizr API, and will run after the API call has returned
+        // "handler": function (tests) {},
+
+        // When parseFiles = true, matchCommunityTests = true will attempt to
+        // match user-contributed tests.
+        "matchCommunityTests" : false,
+
+        // Have custom Modernizr tests? Add paths to their location here.
+        "customTests" : []
+      }
+
+    },
+
 
     // Watches files for changes and runs tasks based on the changed files
     watch: {
@@ -168,7 +232,7 @@ module.exports = function (grunt) {
         ignorePath:  /\.\.\//
       },
       sass: {
-      src: ['<%= yeoman.app %>/styles/{,*/}*.{scss,sass}'],
+        src: ['<%= yeoman.app %>/styles/{,*/}*.{scss,sass}'],
         ignorePath: /(\.\.\/){1,2}bower_components\//
       }
     },
@@ -440,6 +504,7 @@ module.exports = function (grunt) {
 
     grunt.task.run([
       'clean:server',
+      'modernizr:dist',
       'wiredep',
       'concurrent:server',
       'autoprefixer',
@@ -455,6 +520,7 @@ module.exports = function (grunt) {
 
   grunt.registerTask('test', [
     'clean:server',
+    'modernizr:dist',
     'bower:app',
     'replace:test',
     'concurrent:test',
