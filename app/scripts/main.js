fcac428395d0b7524c0ec09543a4bd1504a62056
mjs2020 <me@fm.to.it>
2014-12-12 18:19:11 +0000

    Cleanup to deps in main.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,7 @@
 /*jshint unused: vars */
 require.config({
   paths: {
-    'angular': '../../bower_components/angular/angular',
+    angular: '../../bower_components/angular/angular',
     'angular-animate': '../../bower_components/angular-animate/angular-animate',
     'angular-aria': '../../bower_components/angular-aria/angular-aria',
     'angular-cookies': '../../bower_components/angular-cookies/angular-cookies',
@@ -12,20 +12,22 @@ require.config({
     'angular-sanitize': '../../bower_components/angular-sanitize/angular-sanitize',
     'angular-scenario': '../../bower_components/angular-scenario/angular-scenario',
     'angular-touch': '../../bower_components/angular-touch/angular-touch',
-    'text': '../../bower_components/requirejs-text/text',
-    'bootstrap': '../../bower_components/bootstrap/dist/js/bootstrap',
-    'd3': '../../bower_components/d3/d3',
-    'd3tip': '../../bower_components/d3-tip/index',
-    'lodash': '../../bower_components/lodash/dist/lodash.compat',
-    'jquery': '../../bower_components/jquery/dist/jquery',
+    text: '../../bower_components/requirejs-text/text',
+    bootstrap: '../../bower_components/bootstrap/dist/js/bootstrap',
+    lodash: '../../bower_components/lodash/dist/lodash.compat',
+    'simple-statistics': '../../bower_components/simple-statistics/src/simple_statistics',
+    modernizr: '../../bower_components/modernizr/modernizr',
+    moment: '../../bower_components/momentjs/moment',
+    jquery: '../../bower_components/jquery/dist/jquery',
     'jquery-cookie': '../../bower_components/jquery-cookie/jquery.cookie',
     'jquery-mousewheel': '../../bower_components/jquery-mousewheel/jquery.mousewheel',
+    d3: '../../bower_components/d3/d3',
+    'd3-tip': '../../bower_components/d3-tip/index',
+    data: 'data',
     'pocket-api': 'pocket-api',
-    'oauthpopup': 'oauthpopup',
-    'simple-statistics': '../../bower_components/simple-statistics/src/simple_statistics',
-    'modernizr': '../../bower_components/modernizr/modernizr',
-    'requirejs-text': '../../bower_components/requirejs-text/text',
-    'moment': '../../bower_components/momentjs/moment'
+    oauthpopup: 'oauthpopup',
+    momentjs: '../../bower_components/momentjs/moment',
+    'requirejs-text': '../../bower_components/requirejs-text/text'
   },
   shim: {
     angular: {
@@ -61,12 +63,12 @@ require.config({
       ],
       exports: 'angular.mock'
     },
-    'bootstrap': {
+    bootstrap: {
       deps: [
         'jquery'
       ]
     },
-    'd3tip': {
+    'd3-tip': {
       deps: [
         'd3'
       ]
