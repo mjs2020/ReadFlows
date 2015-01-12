43daefc7a04b8e2ce44798f0de04cbd85488ead0
mjs2020 <me@fm.to.it>
2015-01-12 17:34:00 +0000

    Made the getReadsList much better performing.

--- app/scripts/services/pocketdata.js
+++ app/scripts/services/pocketdata.js
@@ -53,84 +53,97 @@ define(['angular', 'pouchdb', 'lodash'], function (angular, PouchDB, _) {
     }
 
     this.getReadsList = function(accessToken, callback) {
+      if(typeof accessToken === 'undefined') callback('Undefined accessToken');
+
       var url = baseUrl+'proxy.php?a=getReadsList&accessToken='+accessToken,
           state = {};
 
-      DB.info()
-        .then(function (response) {
-          state.initial_items = response.doc_count;
-          return DB.get('ReadFlows.lastUpdate');
-        })
-        .then(function (response) {                   // lasstUpdate is set so we udate the url with the since paramenter
-          url = url+'&since='+response.timestamp;
+      if(DEBUG) console.log('Initiated get read list procedure');
+
+      DB.allDocs({include_docs: true})
+
+        .then(function (response) {                  // Set up state and make http call
+          state.initialCount = response.total_rows;
+          state.dbDocs = response.rows;
+          if(DEBUG) console.log('Number of documents in the local DB before update: '+state.initialCount);
+
+          state.lastUpdate = _.find(state.dbDocs, function (v, i, c) {
+            return v.id == 'ReadFlows.lastUpdate'
+          })
+          state.lastUpdate = state.lastUpdate.doc;
+
+          if (state.lastUpdate.timestamp) url = url+'&since='+state.lastUpdate.timestamp;
+          if(DEBUG && state.lastUpdate.timestamp) console.log('There was no lastUpdate stored so we are fetching all documents from PocketAPI');
+          if(DEBUG && state.lastUpdate.timestamp) console.log('lastUpdate was set to '+state.lastUpdate.timestamp+' so we will fetch any new documents since then.');
+          if(DEBUG) console.log('Going to call: '+url);
           return $http.get(url);
         })
-        .catch(function (error) {                     // lastUpdate is not set so we fetch the whole list by url
-          return $http.get(url);
-        })
-        .then(function success (data, status, headers, config) {
-          state.readData = data.data.list;
-          state.added_items = state.readData.length;
-          state.since = data.data.since;
-          return DB.allDocs()                         // Return promise to read current DB so that in the next step we can merge the two
-        }, function error (data, status, headers, config) {
-          if(DEBUG) console.log('There was an error in the HTTP request to get data.');
-          throw status;                               // If the http call failed throw an error
-        })
-        .then(function (dbItems) {
-          state.readData = _.chain(state.readData)    // We process the retrived data adding _id and _rev attributes getting the _rev from the DB
-                          .toArray()
-                          .map(function (apiDoc, k , c) {
-                            // Check if we already had the document in the DB
-                            var dbDoc = _.find(dbItems.rows, function (dbItem, i, c1) {
-                              if (typeof dbItem.item_id != 'undefined' && typeof apiDoc.item_id != 'undefined') return dbItem.item_id == apiDoc.item_id;
+
+        .then(function success (response) {               // Process http response and prepare db insert
+          state.newData = _.toArray(response.data.list);
+          state.addedItems = state.newData.length;
+          state.since = response.data.since;
+          if(DEBUG) console.log('Data was received from the PocketAPI through the proxy. '+state.addedItems+' items were received.');
+
+          // Add _id and _rev to all the newData items before injecting them so that existing docs update correctly.
+          state.newData = _.map(state.newData, function (apiDoc, k , c) {
+                            var dbDoc = _.find(state.dbDocs, function (dbItem, k1, c1) {
+                              if (typeof dbItem.doc.item_id != 'undefined' && typeof apiDoc.item_id != 'undefined') return dbItem.doc.item_id == apiDoc.item_id;
                               return false;
                             })
-                            // and if we do then get the _id and _rev into the
+                            // and if we do then get the _id and _rev into the document for addition
                             if (typeof dbDoc != 'undefined') {
-                              apiDoc._id = dbDoc._id;
-                              apiDoc._rev = dbDoc._rev;
+                              apiDoc._id = dbDoc.doc._id;
+                              apiDoc._rev = dbDoc.doc._rev;
+                            } else {                 // If it's a new doc then set it's _id anyhow
+                              apiDoc._id = apiDoc.item_id;
                             }
                             return apiDoc;
-                          })
-                          .value();
-          return DB.bulkDocs(state.readData)          // Return promise with injection into pouchdb
+                          });
+          return DB.bulkDocs(state.newData);          // Return promise with injection into pouchdb
+        }, function error (response) {
+          if(DEBUG) console.log('There was an error in the HTTP request to get data.');
+          throw response.status;                               // If the http call failed throw an error
         })
-        .then(function (err, result) {
-          if(DEBUG) console.log('Getting DB info with new total');
-          return DB.info();                           // get db info with new total
+
+        .then(function (response) {              // Save lastUpdate value
+          if (state.lastUpdate._rev) {
+            if(DEBUG) console.log('Insert succeeded. Saving lastUpdate value with new revision.');
+            return DB.put({ timestamp: state.since }, 'ReadFlows.lastUpdate', state.lastUpdate._rev);
+          } else {
+            if(DEBUG) console.log('Insert succeeded. Saving lastUpdate value.');
+            return DB.put({ timestamp: state.since }, 'ReadFlows.lastUpdate');
+          }
         })
-        .then(function (result) {
-          state.final_items = result.doc_count;
-          if(DEBUG) console.log('Getting ReadFlows.lastUpdate');
-          return DB.get('ReadFlows.lastUpdate');
+
+        .then(function (response) {              // Get final DB list
+          if(DEBUG) console.log('lastUpdate saved. Getting final DB element list.');
+          return DB.allDocs({include_docs: true})
         })
-        .then(function (result) {
-          if(DEBUG) console.log('Updating ReadFlows.lastUpdate');
-          return DB.put({                             // lastUpdate exists so we update it with _rev
-            timestamp: state.since
-          }, 'ReadFlows.lastUpdate', result._rev);
+
+        .then(function (response) {              // Final post processing and return
+          state.dbDocs = _.map(response.rows, function (v, i, c) {
+            return v.doc;
+          });
+          state.finalCount = response.total_rows;
+          if(DEBUG) console.log('Number of documents in the local DB after update: '+state.finalCount);
+          state.lastUpdate = _.find(state.dbDocs, function (v, i, c) {
+            return v._id == 'ReadFlows.lastUpdate'
+          })
+          state.lastUpdate = state.lastUpdate.timestamp;
+          delete state.newData;
+          delete state.since;
+          callback(null, state)
         })
-        .catch(function (err) {
-          console.log(err);
-          if(DEBUG) console.log('Setting ReadFlows.lastUpdate which did not exist');
-          return DB.put({                             // lastUpdate does not exist so we create it
-            timestamp: state.since
-          }, 'ReadFlows.lastUpdate');
-        })
-        .then(function (result) {
-          if(DEBUG) console.log('Update should have completed correctly, returning to login process.');
-          // If we got here data has been fetched and inserted into pouchdb and we can call the callback
-          callback(null,state)
-        })
-        .catch(function (error) {
-          // Something went wrong in the http request or in the db insertion
+
+        .catch(function (error) {                     // Something went wrong in the http request or in the db insertion
           if(DEBUG) console.log('Something went wrong in the http request or in the db insertion.');
           callback(error, null);
         });
+
     }
 
-    this.processData = function (callback) {
+    this.processData = function (data, callback) {
       //compute stats if necessary and then return them
     }
 
